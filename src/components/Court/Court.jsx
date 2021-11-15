234a39b2aadf4933feefea1591a25f6de44b5ad2
Chuck Choi <hyunchoi94@gmail.com>
2021-11-15 08:11:12 -0800

    court color change when full

--- src/components/Court/Court.jsx
+++ src/components/Court/Court.jsx
@@ -6,10 +6,10 @@ import JoinCourt from './JoinCourt/JoinCourt';
 function Court(props) {
     const {courtNum, playerList, waitList, handleJoinCourt} = props;
     
-    console.log(courtNum)
+    console.log(playerList)
 
     return (
-        <div className={`${styles.court} ${styles.available}`}>
+        <div className={`${styles.court} ${playerList.length < 4 ? styles.available : styles.occupied}`}>
             <h2 className={styles.courtName}>Court {courtNum}</h2>
             <JoinCourt 
                 handleJoinCourt={handleJoinCourt} 
